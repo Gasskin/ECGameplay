84d8caa6cfab1aa7f1e743831dd70c3bdb6ba5f0
Logarius <415753928@qq.com>
2023-04-19 00:45:15 +0800

    1

--- Assets/ECSCombat/Action/Base/BaseActionExecution.cs
+++
@@ -1,7 +0,0 @@
-public abstract class BaseActionExecution
-{
-    /// 行为对象
-    public GameEntity creator;
-    /// 行为目标
-    public GameEntity target;
-}
--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -2,24 +2,5 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public GameObject playerAsset;
-    public GameObject enemyAsset;
-
-    private BasicHero hero;
     
-    private BasicMonster monster;
-
-    private void Start()
-    {
-        hero = new BasicHero();
-        hero.Move(new Vector3(5,0,0));
-        
-        monster = new BasicMonster();
-        monster.Move(new Vector3(-5,0,0));
-    }
-
-    public void Attack()
-    {
-        hero.Attack(monster.entity);
-    }
 }
